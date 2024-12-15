f6976f39e3b2e4ad61e8961fea84722006a47057
markus-bo <markus.bodlos@outlook.com>
2024-12-15 19:17:16 +0000

    Add solution day 13 part 1

--- AoC_2024.Day13/Program.cs
+++ AoC_2024.Day13/Program.cs
@@ -8,6 +8,12 @@ using System.Threading;
 using System.Runtime.ExceptionServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using AoC_Toolbox.Mathematic;
+
+record ClawMachineConfig(Point ButtonA, Point ButtonB, Point Prize, int TokenACost, int TokenBCost);
 
 internal class Solution
 {
@@ -18,15 +24,168 @@ internal class Solution
 
     static object? solutionPart1(string[] input)
     {
-        var result = 0;
+        var clawMachines = new List<ClawMachineConfig>();
 
-        return result;
+        for (int i = 0; i <= input.Length - 3; i += 4)
+        {
+            var line = input[i];
+
+            var clawMachine = new ClawMachineConfig(
+                ButtonA: ParseButtonInputLine(line),
+                ButtonB: ParseButtonInputLine(input[i + 1]),
+                Prize: ParsePrizeInputLine(input[i + 2]),
+                TokenACost: 3,
+                TokenBCost: 1
+            );
+
+            clawMachines.Add(clawMachine);
+        }
+
+        var totalTokens = 0L;
+
+        foreach (var clawMachine in clawMachines)
+        {
+            var result = MinimizeTokens2(clawMachine, 0, 0, 0, 0);
+
+            if (result == long.MaxValue)
+            {
+                continue;
+            }
+
+            totalTokens += result;
+        }
+
+        return totalTokens;
+    }
+
+    static long MinimizeTokens2(ClawMachineConfig clawMachineConfig, long currentPositionX, long currentPositionY, int cost, int recursionCount)
+    {
+        if (clawMachineConfig.Prize.X == 0 && clawMachineConfig.Prize.Y == 0)
+        {
+            return 0;
+        }
+
+        if (clawMachineConfig.Prize.X % clawMachineConfig.ButtonA.X == 0 &&
+            clawMachineConfig.Prize.Y % clawMachineConfig.ButtonA.Y == 0)
+        {
+            var totalMovementsX = clawMachineConfig.Prize.X / clawMachineConfig.ButtonA.X;
+
+            if (totalMovementsX * clawMachineConfig.ButtonA.Y == clawMachineConfig.Prize.Y)
+            {
+                return totalMovementsX * clawMachineConfig.TokenACost;
+            }
+        }
+
+        if (clawMachineConfig.Prize.X % clawMachineConfig.ButtonB.X == 0 &&
+            clawMachineConfig.Prize.Y % clawMachineConfig.ButtonB.Y == 0)
+        {
+            var totalMovementsX = clawMachineConfig.Prize.X / clawMachineConfig.ButtonB.X;
+
+            if (totalMovementsX * clawMachineConfig.ButtonB.Y == clawMachineConfig.Prize.Y)
+            {
+                return totalMovementsX * clawMachineConfig.TokenBCost;
+            }
+        }
+
+        var minimumTokenCost = long.MaxValue;
+
+        var gcd = new long[] { clawMachineConfig.Prize.X, clawMachineConfig.Prize.Y}
+                        .GetGCD();
+
+        var requiredXDivisor = clawMachineConfig.Prize.X / (double)gcd;
+        var requiredYDivisor = clawMachineConfig.Prize.Y / (double)gcd;
+
+        for (int pressesButtonA = 1; pressesButtonA < 100000; pressesButtonA++)
+        {
+            for (int pressesButtonB = 1; pressesButtonB < 100000; pressesButtonB++)
+            {
+                var movementX = pressesButtonA * clawMachineConfig.ButtonA.X + pressesButtonB * clawMachineConfig.ButtonB.X;
+                var movementY = pressesButtonA * clawMachineConfig.ButtonA.Y + pressesButtonB * clawMachineConfig.ButtonB.Y;
+
+
+                if (movementX / requiredXDivisor == movementY / requiredYDivisor)
+                {
+                    var totalMovementsX = clawMachineConfig.Prize.X / (double)movementX;
+
+                    var totalPressesButtonA = (long)(totalMovementsX * pressesButtonA);
+                    var totalPressesButtonB = (long)(totalMovementsX * pressesButtonB);
+
+                    minimumTokenCost = Math.Min(minimumTokenCost, totalPressesButtonA * clawMachineConfig.TokenACost + totalPressesButtonB * clawMachineConfig.TokenBCost);
+
+                    return minimumTokenCost;
+                }
+            }
+        }
+
+        return minimumTokenCost;
+    }
+
+    static Point ParseButtonInputLine(string line)
+    {
+        var split = line.Split(": ");
+        var split2 = split[1].Split(", ")
+                             .Select(x => x.Split("+")[1])
+                             .Select(int.Parse)
+                             .ToList();
+
+        return new Point(x: split2[0], y: split2[1]);
+    }
+
+    static Point ParsePrizeInputLine(string line)
+    {
+        var split = line.Split(": ");
+        var split2 = split[1].Split(", ")
+                             .Select(x => x.Split("=")[1])
+                             .Select(int.Parse)
+                             .ToList();
+
+        return new Point(x: split2[0], y: split2[1]);
+    }
+
+    static Point ParsePrizeInputLineWithHighCost(string line)
+    {
+        var split = line.Split(": ");
+        var split2 = split[1].Split(", ")
+                             .Select(x => x.Split("=")[1])
+                             .Select(int.Parse)
+                             .ToList();
+
+        return new Point(x: split2[0] + 10000000000000L, y: split2[1] + 10000000000000L);
     }
 
     static object? solutionPart2(string[] input)
     {
-        var result = 0;
+                var clawMachines = new List<ClawMachineConfig>();
 
-        return result;
+        for (int i = 0; i <= input.Length - 3; i += 4)
+        {
+            var line = input[i];
+
+            var clawMachine = new ClawMachineConfig(
+                ButtonA: ParseButtonInputLine(line),
+                ButtonB: ParseButtonInputLine(input[i + 1]),
+                Prize: ParsePrizeInputLineWithHighCost(input[i + 2]),
+                TokenACost: 3,
+                TokenBCost: 1
+            );
+
+            clawMachines.Add(clawMachine);
+        }
+
+        var totalTokens = 0L;
+
+        foreach (var clawMachine in clawMachines)
+        {
+            var result = MinimizeTokens2(clawMachine, 0, 0, 0, 0);
+
+            if (result == long.MaxValue)
+            {
+                continue;
+            }
+
+            totalTokens += result;
+        }
+
+        return totalTokens;
     }
 }
